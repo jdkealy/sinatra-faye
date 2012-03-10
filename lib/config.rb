b22f6e78aa4d6b1bd81f4d2466a11e6fa44910fc
john <john@john-Pangolin-Performance.(none)>
2012-03-10 00:57:05 -0500

    no need for mongo connection yet

--- lib/config.rb
+++ lib/config.rb
@@ -16,24 +16,24 @@ require 'yajl'
 #require 'nokogiri'
 
 
-mongoid_conn = Mongo::Connection.new 'localhost', 27017, :pool_size => 10
-Mongoid.configure do |c|
-  c.max_retries_on_connection_failure = 0
-  # c.skip_version_check = true
-  c.identity_map_enabled = false
-
-  begin
-    db_name = 'bidder_01'
-
-    if ENV['RACK_ENV']  && ENV['RACK_ENV'] == 'test'
-      db_name = 'bidder_test'
-    end
-    c.master = mongoid_conn.db(db_name)
-
-  rescue Exception=>err
-    abort "An error occurred while creating the mongoid connection pool: #{err}"
-  end
-end
-
-# load all models
-Dir.glob(File.expand_path(File.dirname(__FILE__)) + '/models/*.rb') {|file| require file}
+#mongoid_conn = Mongo::Connection.new 'localhost', 27017, :pool_size => 10
+#Mongoid.configure do |c|
+#  c.max_retries_on_connection_failure = 0
+#  # c.skip_version_check = true
+#  c.identity_map_enabled = false
+#
+#  begin
+#    db_name = 'bidder_01'
+#
+#    if ENV['RACK_ENV']  && ENV['RACK_ENV'] == 'test'
+#      db_name = 'bidder_test'
+#    end
+#    c.master = mongoid_conn.db(db_name)
+#
+#  rescue Exception=>err
+#    abort "An error occurred while creating the mongoid connection pool: #{err}"
+#  end
+#end
+#
+## load all models
+#Dir.glob(File.expand_path(File.dirname(__FILE__)) + '/models/*.rb') {|file| require file}
